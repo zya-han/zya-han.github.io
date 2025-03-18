f64d37684cf4aced38515bd98b09857da2d675e0
주아 <141071838+zya-han@users.noreply.github.com>
2025-03-18 15:36:32 +0900

    Update lunrsearchengine.js

--- assets/js/lunrsearchengine.js
+++ assets/js/lunrsearchengine.js
@@ -22,14 +22,14 @@ var documents = [{% for page in site.pages %}{% if page.url contains '.xml' or p
     }{% if forloop.last %}{% else %}, {% endif %}{% endfor %}];
 
 var idx = lunr(function () {
-    this.use(lunr.multiLanguage('en', 'ko'));
-    this.ref('id');
-    this.field('title');
-    this.field('body');
+    this.use(lunr.multiLanguage('en', 'ko', 'zh'))
+    this.ref('id')
+    this.field('title')
+    this.field('body')
 
     documents.forEach(function (doc) {
         this.add(doc)
-    }, this);
+    }, this)
 });
 function lunr_search(term) {
     document.getElementById('lunrsearchresults').innerHTML = '<ul></ul>';
