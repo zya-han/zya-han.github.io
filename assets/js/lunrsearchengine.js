00f7322b44f901ad55b2f9c5b8631ea752a1d9b4
주아 <141071838+zya-han@users.noreply.github.com>
2025-03-18 15:15:25 +0900

    Update lunrsearchengine.js

--- assets/js/lunrsearchengine.js
+++ assets/js/lunrsearchengine.js
@@ -21,16 +21,15 @@ var documents = [{% for page in site.pages %}{% if page.url contains '.xml' or p
     "body": "{{ page.date | date: "%Y/%m/%d" }} - {{ page.content | markdownify | replace: '.', '. ' | replace: '</h2>', ': ' | replace: '</h3>', ': ' | replace: '</h4>', ': ' | replace: '</p>', ' ' | strip_html | strip_newlines | replace: '  ', ' ' | replace: '"', ' ' }}"{% assign counter = counter | plus: 1 %}
     }{% if forloop.last %}{% else %}, {% endif %}{% endfor %}];
 
-var idx = lunr(function () {
-    this.use(lunr.multiLanguage('en', 'ko', 'zh'));
-    this.ref('id')
-    this.field('title')
-    this.field('body')
 
-    documents.forEach(function (doc) {
-        this.add(doc)
-    }, this)
-});
+var idx = new lunr.Index;
+idx.field('id');
+idx.field('title', { boost: 10 });
+idx.field('body');
+
+documents.forEach(function (doc) {
+    this.add(doc)
+}, this);
 function lunr_search(term) {
     document.getElementById('lunrsearchresults').innerHTML = '<ul></ul>';
     if(term) {
