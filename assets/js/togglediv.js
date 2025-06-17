d552546a5045f2bb1627aa6956c56c468fa34aa9
zya-han <chiclaxica@gmail.com>
2025-06-17 20:17:32 +0000

    deploy: zya-han/zya-han.github.io-private@d3a35b05263455002ad24680d21eda94ce8e1586

---
+++ assets/js/togglediv.js
@@ -0,0 +1,8 @@
+function HideShowElement() {
+    var x = document.getElementById("HideShow");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
